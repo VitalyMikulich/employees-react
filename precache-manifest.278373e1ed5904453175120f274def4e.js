b93cc010fb58cdf6c79119f19bb88e448186bc7e
VitalyMikulich <mr.wital1999@gmail.com>
2020-05-07 17:48:27 +0300

    Updates

---
+++ precache-manifest.278373e1ed5904453175120f274def4e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "80b022d37a228eda253537e0ea037d19",
+    "url": "/employees-react/index.html"
+  },
+  {
+    "revision": "cb9f457683c3f585afb3",
+    "url": "/employees-react/static/css/main.ef6e5725.chunk.css"
+  },
+  {
+    "revision": "9c7500cde17729ab6f9c",
+    "url": "/employees-react/static/js/2.1af99a7d.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/employees-react/static/js/2.1af99a7d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "cb9f457683c3f585afb3",
+    "url": "/employees-react/static/js/main.c1967d9b.chunk.js"
+  },
+  {
+    "revision": "e2a253747d828e11b18e",
+    "url": "/employees-react/static/js/runtime-main.8933ce88.js"
+  }
+]);
